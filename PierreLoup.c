66e925941a045ede5b0491faff7b54f01be1f456
Steinwolf <pierreloup.martigne@viaces.fr>
2017-10-26 16:23:23 +0200

    V1.1

--- PierreLoup.c
+++ PierreLoup.c
@@ -1,1 +1,8 @@
+#include "PierreLoup.h"
 
+int fonctionAddition(double nombre1,double nombre2)
+{
+    double sortie;
+    sortie = nombre1 + nombre2;
+    return sortie;
+}
--- main.c
+++ main.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "Adrien.h"
+#include "Alexandre.h"
+#include "Corentin.h"
+#include "Francois.h"
+#include "PierreLoup.h"
+
 
 int main()
 {
